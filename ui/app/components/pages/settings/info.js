97e28d0d00a3b38788ddeee2191b95361431ec42
Gabriel Cardona <gabriel@bitcoin.com>
2018-09-30 23:30:14 -0700

    Branding and remove old UI.

--- ui/app/components/pages/settings/info.js
+++ ui/app/components/pages/settings/info.js
@@ -12,91 +12,97 @@ class Info extends Component {
   }
 
   renderLogo () {
-    return (
-      h('div.settings__info-logo-wrapper', [
-        h('img.settings__info-logo', { src: 'images/info-logo.png' }),
-      ])
-    )
+    return h('div.settings__info-logo-wrapper', [
+      h('img.settings__info-logo', { src: 'images/bch_logo.svg' }),
+    ])
   }
 
   renderInfoLinks () {
-    return (
-      h('div.settings__content-item.settings__content-item--without-height', [
+    return h(
+      'div.settings__content-item.settings__content-item--without-height',
+      [
         h('div.settings__info-link-header', this.context.t('links')),
         h('div.settings__info-link-item', [
-          h('a', {
-            href: 'https://badgerwallet.cash/privacy.html',
-            target: '_blank',
-          }, [
-            h('span.settings__info-link', this.context.t('privacyMsg')),
-          ]),
+          h(
+            'a',
+            {
+              href: 'https://badgerwallet.cash/privacy.html',
+              target: '_blank',
+            },
+            [h('span.settings__info-link', this.context.t('privacyMsg'))]
+          ),
         ]),
         h('div.settings__info-link-item', [
-          h('a', {
-            href: 'https://badgerwallet.cash/terms.html',
-            target: '_blank',
-          }, [
-            h('span.settings__info-link', this.context.t('terms')),
-          ]),
+          h(
+            'a',
+            {
+              href: 'https://badgerwallet.cash/terms.html',
+              target: '_blank',
+            },
+            [h('span.settings__info-link', this.context.t('terms'))]
+          ),
         ]),
         h('div.settings__info-link-item', [
-          h('a', {
-            href: 'https://badgerwallet.cash/attributions.html',
-            target: '_blank',
-          }, [
-            h('span.settings__info-link', this.context.t('attributions')),
-          ]),
+          h(
+            'a',
+            {
+              href: 'https://badgerwallet.cash/attributions.html',
+              target: '_blank',
+            },
+            [h('span.settings__info-link', this.context.t('attributions'))]
+          ),
         ]),
         h('hr.settings__info-separator'),
         h('div.settings__info-link-item', [
-          h('a', {
-            href: 'https://support.metamask.io',
-            target: '_blank',
-          }, [
-            h('span.settings__info-link', this.context.t('supportCenter')),
-          ]),
+          h(
+            'a',
+            {
+              href: 'https://support.metamask.io',
+              target: '_blank',
+            },
+            [h('span.settings__info-link', this.context.t('supportCenter'))]
+          ),
         ]),
         h('div.settings__info-link-item', [
-          h('a', {
-            href: 'https://badgerwallet.cash/',
-            target: '_blank',
-          }, [
-            h('span.settings__info-link', this.context.t('visitWebSite')),
-          ]),
+          h(
+            'a',
+            {
+              href: 'https://badgerwallet.cash/',
+              target: '_blank',
+            },
+            [h('span.settings__info-link', this.context.t('visitWebSite'))]
+          ),
         ]),
         h('div.settings__info-link-item', [
-          h('a', {
-            target: '_blank',
-            href: 'mailto:help@metamask.io?subject=Feedback',
-          }, [
-            h('span.settings__info-link', this.context.t('emailUs')),
-          ]),
+          h(
+            'a',
+            {
+              target: '_blank',
+              href: 'mailto:help@metamask.io?subject=Feedback',
+            },
+            [h('span.settings__info-link', this.context.t('emailUs'))]
+          ),
         ]),
-      ])
+      ]
     )
   }
 
   render () {
-    return (
-      h('div.settings__content', [
-        h('div.settings__content-row', [
-          h('div.settings__content-item.settings__content-item--without-height', [
-            this.renderLogo(),
-            h('div.settings__info-item', [
-              h('div.settings__info-version-header', 'Badger Version'),
-              h('div.settings__info-version-number', this.state.version),
-            ]),
-            h('div.settings__info-item', [
-              h(
-                'div.settings__info-about',
-                this.context.t('builtInCalifornia')
-              ),
-            ]),
+    return h('div.settings__content', [
+      h('div.settings__content-row', [
+        h('div.settings__content-item.settings__content-item--without-height', [
+          this.renderLogo(),
+          h('div.settings__info-item', [
+            h('div.settings__info-version-header', 'Badger Version'),
+            h('div.settings__info-version-number', this.state.version),
           ]),
-          this.renderInfoLinks(),
+          h('div.settings__info-item', [
+            h('div.settings__info-about', this.context.t('builtInCalifornia')),
+          ]),
         ]),
-      ])
-    )
+        this.renderInfoLinks(),
+      ]),
+    ])
   }
 }
 

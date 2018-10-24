ef578b4bde6f2097d582eb9f9c88eda30cbe832c
SpendBCH <SpendBCH@SpendBCH.io>
2018-10-24 01:18:13 -0700

    decode slp txs

--- app/scripts/controllers/transactions/slp-utils.js
+++ app/scripts/controllers/transactions/slp-utils.js
@@ -6,7 +6,7 @@ class SlpUtils {
 
     static get lokadIdHex() { return "534c5000" }
 
-    static decodeTxOut(txDetails) {
+    static decodeMetadata(txDetails) {
         // txOut = {
         //     txid: 
         //     tx: {} //transaction details from bitbox object
@@ -36,13 +36,80 @@ class SlpUtils {
             out.token = txDetails.txid;
             out.ticker = Buffer.from(script[4], 'hex').toString('ascii')
             out.name = Buffer.from(script[5], 'hex').toString('ascii')
-            out.quantity = new BigNumber(script[10], 16);
+            out.decimals = script[8].startsWith('OP_') ? parseInt(script[8].slice(3), 16) : parseInt(script[8], 16)
+            out.quantity = new BigNumber(script[10], 16)
         } else {
             throw new Error('Invalid tx type');
         }
 
         return out;
     }
+
+    static decodeTxOut(txOut) {
+      // txOut = {
+      //     txid: 
+      //     tx: {} //transaction details from bitbox object
+      // }
+      let out = {
+        token: '',
+        quantity: 0,
+        baton: false,
+      }
+
+      const vout = parseInt(txOut.vout)
+
+      const script = BITBOX.Script.toASM(Buffer.from(txOut.tx.vout[0].scriptPubKey.hex, 'hex')).split(' ')
+
+      if (script[0] !== 'OP_RETURN') {
+        throw new Error('Not an OP_RETURN')
+      }
+
+      if (script[1] !== this.lokadIdHex) {
+        throw new Error('Not a SLP OP_RETURN')
+      }
+
+      if (script[2] != 'OP_1') { // NOTE: bitcoincashlib-js converts hex 01 to OP_1 due to BIP62.3 enforcement
+        throw new Error('Unknown token type')
+      }
+
+      const type = Buffer.from(script[3], 'hex').toString('ascii').toLowerCase()
+
+      if (type === 'genesis') {
+        if (script[9] === 'OP_2' && vout === 2 || parseInt(script[9], 16) === vout) {
+          out.token = txOut.txid
+          out.baton = true
+          return out
+        }
+        if (vout !== 1) {
+          throw new Error('Not a SLP txout')
+        }
+        out.token = txOut.txid
+        out.quantity = new BigNumber(script[10], 16)
+      } else if (type === 'mint') {
+        if (script[5] === 'OP_2' && vout === 2 || parseInt(script[5], 16) === vout) {
+          out.token = script[4]
+          out.baton = true
+          return out
+        }
+
+        if (txOut.vout !== 1) {
+          throw new Error('Not a SLP txout')
+        }
+        out.token = script[4]
+        out.quantity = new BigNumber(script[6], 16)
+      } else if (type === 'send') {
+        if (script.length <= vout + 4) {
+          throw new Error('Not a SLP txout')
+        }
+
+        out.token = script[4]
+        out.quantity = new BigNumber(script[vout + 4], 16)
+      } else {
+        throw new Error('Invalid tx type')
+      }
+
+      return out
+  }
 }
 
 module.exports = SlpUtils

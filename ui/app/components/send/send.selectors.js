fb60b6e6da00e692aade3a17ec13d002dfaa4d97
SpendBCH <SpendBCH@SpendBCH.io>
2018-11-04 14:52:19 -0700

    Get selected token method

--- ui/app/components/send/send.selectors.js
+++ ui/app/components/send/send.selectors.js
@@ -170,9 +170,16 @@ function getSelectedIdentity (state) {
 }
 
 function getSelectedToken (state) {
-  const tokens = state.metamask.tokens || []
+  const selectedAddress = getSelectedAddress(state)
   const selectedTokenAddress = state.metamask.selectedTokenAddress
-  const selectedToken = tokens.filter(({ address }) => address === selectedTokenAddress)[0]
+  const providerType = state.metamask.provider.type
+  const addressAccountTokens = state.metamask.accountTokens[selectedAddress]
+  const accountTokens = addressAccountTokens ? addressAccountTokens[providerType] : []
+  const selectedToken = accountTokens.filter(({ address, string }) => { 
+    // TODO: filter for mint baton by properties
+    if (string === 'Mint Baton') return false
+    return address === selectedTokenAddress
+  })[0]
   const sendToken = state.metamask.send.token
 
   return selectedToken || sendToken || null

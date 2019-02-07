5afa06d85441c389f91f5cc04cfc18d618145846
Peter Zenger <peter.zenger1@gmail.com>
2019-02-07 14:28:50 -0800

    Minor cleanup

--- ui/app/components/token-cell.js
+++ ui/app/components/token-cell.js
@@ -3,7 +3,6 @@ const h = require('react-hyperscript')
 const inherits = require('util').inherits
 const connect = require('react-redux').connect
 const Identicon = require('./identicon')
-const prefixForNetwork = require('../../lib/etherscan-prefix-for-network')
 const selectors = require('../selectors')
 const actions = require('../actions')
 const { conversionUtil, multiplyCurrencies } = require('../conversion-util')
@@ -165,33 +164,4 @@ TokenCell.prototype.render = function () {
       // }, 'SEND'),
     ]
   )
-}
-
-// TokenCell.prototype.send = function (address, event) {
-//   event.preventDefault()
-//   event.stopPropagation()
-//   const url = tokenFactoryFor(address)
-//   if (url) {
-//     navigateTo(url)
-//   }
-// }
-
-TokenCell.prototype.view = function (address, userAddress, network, event) {
-  const url = etherscanLinkFor(address, userAddress, network)
-  if (url) {
-    navigateTo(url)
-  }
-}
-
-function navigateTo (url) {
-  global.platform.openWindow({ url })
-}
-
-function etherscanLinkFor (tokenAddress, address, network) {
-  const prefix = prefixForNetwork(network)
-  return `https://${prefix}etherscan.io/token/${tokenAddress}?a=${address}`
-}
-
-function tokenFactoryFor (tokenAddress) {
-  return `https://tokenfactory.surge.sh/#/token/${tokenAddress}`
 }

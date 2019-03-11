5849cfadeba7ad41610f30b1665c7a1de18f6cd5
Unknown <paoolkim@gmail.com>
2019-03-10 19:54:31 -0700

    validate recipient address
    check against SLP/BCH/cashaccount

--- ui/app/components/send/send-content/send-to-row/send-to-row.component.js
+++ ui/app/components/send/send-content/send-to-row/send-to-row.component.js
@@ -4,6 +4,10 @@ import SendRowWrapper from '../send-row-wrapper/'
 import EnsInput from '../../../ens-input'
 import { getToErrorObject } from './send-to-row.utils.js'
 
+const cashAccountRegex = /^([a-zA-Z0-9_]+)(#([0-9]+)(([0-9]+))).([0-9]+)?$/i
+const cashAddrRegex = /^((bitcoincash:)?(q|p)[a-z0-9]{38,})$/gi
+const slpRegex = /^((simpleledger:)?(q|p)[a-z0-9]{38,})$/gi
+
 export default class SendToRow extends Component {
   static propTypes = {
     closeToDropdown: PropTypes.func,
@@ -22,9 +26,34 @@ export default class SendToRow extends Component {
     t: PropTypes.func,
   }
 
+  //
+  componentDidMount () {
+    this.handleToChange('', '', '')
+  }
+
+  validateAddress (string) {
+    if (cashAccountRegex.test(string)) {
+      return true
+    }
+    if (cashAddrRegex.test(string)) {
+      return true
+    }
+    if (slpRegex.test(string)) {
+      return true
+    }
+    return false
+  }
+
   handleToChange (to, nickname = '', toError) {
+
     const { updateSendTo, updateSendToError } = this.props
-    const toErrorObject = getToErrorObject(to, toError)
+    let toErrorObject = getToErrorObject(to, toError)
+    const valid = this.validateAddress(to)
+
+    if (!valid) {
+      toErrorObject = getToErrorObject(to, 'invalid address')
+    }
+
     updateSendTo(to, nickname)
     updateSendToError(toErrorObject)
     if (toErrorObject.to === null) {

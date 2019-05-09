58bb4119f8c517816f1673bb21174a4a11dee1e6
Unknown <paoolkim@gmail.com>
2019-05-09 10:14:26 -0700

    cleanup
    add method for pending registrations

--- app/scripts/lib/cashaccountutils.js
+++ app/scripts/lib/cashaccountutils.js
@@ -4,12 +4,15 @@ const accounts = localStorage.get('cashaccounts')
 const registrations = localStorage.get('cashaccount-registrations')
 
 class CashAccountUtils {
+  // persist identifer data to localstorage when registration is confirmed
   static async upsertAccounts () {
+    if (registrations === undefined) {
+      return
+    }
+
     for (const each of registrations) {
       const account = await this.getIdentity(each.txid)
-
       account.txid = each.txid
-
       const exists = this.checkExistsInStorageByTxid(each.txid)
       if (!exists) {
         this.saveAccount(account)
@@ -17,30 +20,54 @@ class CashAccountUtils {
     }
   }
 
+  // get identifier data for confirmed cashaccounts
   static async getIdentity (txid) {
     const parsed = await cashaccount.accountLookupViaTxid(txid)
+    if (parsed === undefined) {
+      return
+    }
     const account = await cashaccount.parseBitdbObject(parsed)
     return account
   }
+
+  // get identifier data for unconfirmed registration
+  static async getPendingIdentity (txid) {
+    const parsed = await cashaccount.registrationLookupViaTxid(txid)
+    if (parsed === undefined) {
+      return
+    }
+
+    const account = await cashaccount.parseBitdbObject(parsed)
+    return account
+  }
+
+  // persist identity to localstorage
   static saveAccount (obj) {
     const array = accounts === undefined ? [] : accounts
-
     array.push(obj)
-
     localStorage.set('cashaccounts', array)
   }
+
+  // persist registration txid
   static saveRegistration (obj) {
-    const array = accounts === undefined ? [] : accounts
-
+    const array = registrations === undefined ? [] : registrations
     array.push(obj)
-
     localStorage.set('cashaccount-registrations', array)
   }
 
-  static async checkRegistrations (selectedAddress) {
+  // used to limit to one registration per badger account
+  static async checkIsRegistered (selectedAddress) {
+    if (registrations === undefined) {
+      return
+    }
+
     for (const x of registrations) {
       const { txid } = x
-      const account = await this.getIdentity(txid)
+      const account = await this.getPendingIdentity(txid)
+      if (account === undefined) {
+        return false
+      }
+
       const bchRegistration = account.information.payment[0].address
       if (bchRegistration === selectedAddress) {
         return true
@@ -49,6 +76,7 @@ class CashAccountUtils {
       }
     }
   }
+
   static checkExistsInStorageByTxid (txid) {
     if (accounts !== undefined) {
       const match = accounts.find(x => x.txid === txid)
@@ -56,6 +84,7 @@ class CashAccountUtils {
     }
     return false
   }
+
   static checkExistsInStorageByAddr (bchAddr, slpAddr) {
     if (slpAddr) {
       slpAddr = cashaccount.toSlpAddress(slpAddr)
@@ -73,17 +102,18 @@ class CashAccountUtils {
         return true
       }
 
-      // if (bchRegistration === bchAddr && slpRegistration === slpAddr) {
-      //   return true
-      // }
       return false
     }
   }
+
+  // retrieve identifier object for associated wallet
   static getAccountByAddr (bchAddr, slpAddr) {
     if (slpAddr) {
       slpAddr = cashaccount.toSlpAddress(slpAddr)
     }
-
+    if (accounts === undefined) {
+      return
+    }
     for (const x of accounts) {
       const { payment } = x.information
       const bchRegistration = payment[0].address
@@ -92,9 +122,28 @@ class CashAccountUtils {
       if (bchRegistration === bchAddr) {
         return x
       }
-      // if (bchRegistration === bchAddr && slpRegistration === slpAddr) {
-      //   return x
-      // }
+    }
+  }
+
+  // used to get information on an unconfirmed account
+  static async getRegistrationByAddr (bchAddr) {
+    if (registrations === undefined) {
+      return
+    }
+    for (const x of registrations) {
+      const { txid } = x
+
+      const account = await cashaccount.registrationLookupViaTxid(txid)
+      if (account === undefined) {
+        return
+      }
+
+      const payment = await cashaccount.parsePaymentInfo(account.opreturn)
+
+      const bchRegistration = payment[0].address
+      if (bchRegistration === bchAddr) {
+        return x
+      }
     }
   }
 }

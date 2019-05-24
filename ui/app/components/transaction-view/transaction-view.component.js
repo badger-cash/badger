a701af2130b1e0223b0078dd794484e4e854c199
Unknown <paoolkim@gmail.com>
2019-05-24 15:36:47 -0700

    prompt seed backup

--- ui/app/components/transaction-view/transaction-view.component.js
+++ ui/app/components/transaction-view/transaction-view.component.js
@@ -4,19 +4,65 @@ import Media from 'react-media'
 import MenuBar from '../menu-bar'
 import TransactionViewBalance from '../transaction-view-balance'
 import TransactionList from '../transaction-list'
+import { REVEAL_SEED_ROUTE } from '../../../../ui/app/routes'
+import localStorage from 'store'
 
 export default class TransactionView extends PureComponent {
   static contextTypes = {
     t: PropTypes.func,
   }
 
+  static propTypes = {
+    accountUtxoCache: PropTypes.object,
+    historicalBchTransactions: PropTypes.object,
+    historicalSlpTransactions: PropTypes.object,
+    selectedAddress: PropTypes.string,
+    selectedSlpAddress: PropTypes.string,
+    history: PropTypes.object,
+  }
+
+  renderSeedWarning = () => {
+    const {
+      accountUtxoCache,
+      historicalBchTransactions,
+      historicalSlpTransactions,
+      selectedAddress,
+      selectedSlpAddress,
+      history,
+    } = this.props
+
+    const warn =
+      accountUtxoCache[selectedAddress].length > 0 ||
+      accountUtxoCache[selectedSlpAddress].length > 0 ||
+      historicalBchTransactions[selectedAddress].length > 0 ||
+      historicalSlpTransactions[selectedAddress].length > 0
+
+    if (warn) {
+      return (
+        <div
+          className="seed-warning"
+          onClick={() => history.push(REVEAL_SEED_ROUTE)}
+        >
+          <p> Wallet is not backed up </p>
+        </div>
+      )
+    }
+    return null
+  }
+
   render () {
+    const seedBackedUp = localStorage.get('seedwordsBackedUp')
+
     return (
       <div className="transaction-view">
         <Media query="(max-width: 575px)" render={() => <MenuBar />} />
+        {seedBackedUp !== undefined && seedBackedUp
+          ? ''
+          : this.renderSeedWarning()}
         <div className="transaction-view__balance-wrapper">
           <TransactionViewBalance />
         </div>
+
         <TransactionList />
       </div>
     )

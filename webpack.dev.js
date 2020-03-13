b3c71eedca21e6da0c186961afa07c2c2ffe6ba2
Unknown <paoolkim@gmail.com>
2020-03-12 20:40:06 -0700

    root files cleanup

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-    mode: "development",
-    devtool: "inline-source-map",
+  mode: "development",
+  devtool: "inline-source-map"
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-    mode: "production",
+  mode: "production"
 });
